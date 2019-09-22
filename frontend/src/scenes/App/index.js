220ab797342646d6513390e49e146cb5643f4b81
George Gkasdrogkas <georgegkas@gmail.com>
2019-09-22 22:54:29 +0300

    refactor: merge mechanism and presentation in App

--- frontend/src/scenes/App/index.js
+++ frontend/src/scenes/App/index.js
@@ -1,1 +1,89 @@
-export { default } from './mechanism'
+/**
+ * Globals
+ */
+import React, { useState } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+/**
+ * UI Framework
+ */
+import Slide from '@material-ui/core/Slide'
+
+/**
+ * Ducks
+ */
+import { operations as editorOperations } from './organisms/Editor/duck'
+
+/**
+ * Components
+ */
+import Editor from './organisms/Editor'
+import Dijkstra from './organisms/Dijkstra'
+import Overlay from '../../atoms/Overlay'
+import Dashboard from './organisms/Dashboard'
+import AppBar from './organisms/AppBar'
+import EditorBar from './organisms/EditorBar'
+import PropertiesEditor from './organisms/PropertiesEditor'
+
+const mapStateToProps = state => ({
+  selectedNode: state.editor.present.selectedNode,
+  selectedArrow: state.editor.present.selectedArrow,
+  editorActionType: state.editor.present.editorActionType,
+  isMultiSelect: state.editor.isMultiSelect,
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(editorOperations, dispatch)
+
+const Mechanism = ({
+  isMultiSelect,
+  selectedNode,
+  editorActionType,
+  selectedArrow,
+}) => {
+  const [grid, makeGridVisible] = useState(false)
+  const [dashboard, makeDashboardVisible] = useState(false)
+
+  const toggleGrid = () => makeGridVisible(!grid)
+  const toggleDashboard = () => makeDashboardVisible(!dashboard)
+  const shouldRenderPropertiesEditor =
+    editorActionType === 'select' &&
+    !isMultiSelect &&
+    (selectedArrow.length ^ selectedNode.length) === 1
+  const showAlgorithmPanel = editorActionType === 'isPlaying'
+
+  return (
+    <React.Fragment>
+      {dashboard ? (
+        <React.Fragment>
+          <Dashboard toggleDashboard={toggleDashboard} />
+          <Overlay />
+        </React.Fragment>
+      ) : null}
+
+      <div style={{ position: 'relative' }}>
+        <AppBar toggleDashboard={toggleDashboard} />
+        <EditorBar toggleGrid={toggleGrid} grid={grid} />
+      </div>
+
+      <Editor grid={grid} />
+
+      {shouldRenderPropertiesEditor ? <PropertiesEditor /> : null}
+
+      <Slide
+        direction="left"
+        in={showAlgorithmPanel}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Dijkstra />
+      </Slide>
+    </React.Fragment>
+  )
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Mechanism)
