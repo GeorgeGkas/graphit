2d066b39b144f0d17b6229a063c79a08db04ba64
George Gkasdrogkas <georgegkas@gmail.com>
2019-09-22 23:50:23 +0300

    refactor: merge mechanism and presentation of PropertiesEditor component

--- frontend/src/scenes/App/organisms/PropertiesEditor/index.js
+++ frontend/src/scenes/App/organisms/PropertiesEditor/index.js
@@ -1,1 +1,137 @@
-export { default } from './mechanism'
+/**
+ * Import globals.
+ */
+import React, { useState } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+/**
+ * Import UI framework modules.
+ */
+import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/DeleteSharp'
+import EditIcon from '@material-ui/icons/EditSharp'
+import IconButton from '@material-ui/core/IconButton'
+import { makeStyles } from '@material-ui/core/styles'
+
+/**
+ * Import ducks.
+ */
+import { operations as editorOperations } from '../../organisms/Editor/duck'
+
+/**
+ * Import components.
+ */
+import EdgeEditor from './organisms/EdgeEditor'
+import NodeEditor from './organisms/NodeEditor'
+import { Wrapper } from './styles'
+
+/**
+ * Construct component styles.
+ */
+const useStyles = makeStyles(theme => ({
+  buttonIcon: {
+    fontSize: 20,
+    marginRight: theme.spacing(1),
+  },
+  deleteButton: {
+    borderRadius: 0,
+  },
+  editButton: {
+    borderRadius: 0,
+    margin: theme.spacing(1),
+  },
+}))
+
+/**
+ * Connect component to Redux.
+ */
+const mapStateToProps = state => ({
+  allSelectedEdges: state.editor.present.selectedArrow,
+  allSelectedNodes: state.editor.present.selectedNode,
+  currentStageScale: state.editor.present.scaleStage,
+  edges: state.editor.present.connected,
+  selectedEdge:
+    state.editor.present.connected[state.editor.present.selectedArrow[0]],
+  selectedNode:
+    state.editor.present.nodes[state.editor.present.selectedNode[0]],
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ ...editorOperations }, dispatch)
+
+/**
+ * Component.
+ */
+const PropertiesEditor = ({
+  allSelectedEdges,
+  allSelectedNodes,
+  currentStageScale,
+  deleteArrow: deleteEdge,
+  deleteNode,
+  deleteShape,
+  edges,
+  selectedEdge,
+  selectedNode,
+}) => {
+  const classes = useStyles()
+  const [editorDialog, makeEditorDialogVisible] = useState(false)
+
+  const toggleEditorDialog = () => makeEditorDialogVisible(!editorDialog)
+
+  const deleteShapes = () => {
+    deleteShape()
+
+    for (const node of allSelectedNodes) {
+      deleteNode(node)
+    }
+    for (const arrow of allSelectedEdges) {
+      deleteEdge(arrow)
+    }
+  }
+
+  return (
+    <Wrapper
+      edges={Object.values(edges)}
+      currentStageScale={currentStageScale}
+      selectedEdge={selectedEdge}
+      selectedNode={selectedNode}
+    >
+      <Button
+        className={classes.editButton}
+        color="secondary"
+        onClick={toggleEditorDialog}
+        size="small"
+        variant="contained"
+      >
+        <EditIcon className={classes.buttonIcon} />
+        Edit
+      </Button>
+
+      <IconButton
+        className={classes.deleteButton}
+        color="secondary"
+        onClick={deleteShapes}
+        size="small"
+        variant="contained"
+      >
+        <DeleteIcon />
+      </IconButton>
+
+      {selectedNode ? (
+        <NodeEditor handleClose={toggleEditorDialog} open={editorDialog} />
+      ) : (
+        <EdgeEditor
+          handleClickOpen={toggleEditorDialog}
+          handleClose={toggleEditorDialog}
+          open={editorDialog}
+        />
+      )}
+    </Wrapper>
+  )
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PropertiesEditor)
--- frontend/src/scenes/App/organisms/PropertiesEditor/styles.js
+++ frontend/src/scenes/App/organisms/PropertiesEditor/styles.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
 const getEdgeCenter = props => {
-  let secondExist = props.arrows.some(
+  let secondExist = props.edges.some(
     arr =>
-      arr.from.id === props.selectedArrow.to.id &&
-      arr.to.id === props.selectedArrow.from.id,
+      arr.from.id === props.selectedEdge.to.id &&
+      arr.to.id === props.selectedEdge.from.id,
   )
 
-  const dx = props.selectedArrow.to.x - props.selectedArrow.from.x
-  const dy = props.selectedArrow.to.y - props.selectedArrow.from.y
+  const dx = props.selectedEdge.to.x - props.selectedEdge.from.x
+  const dy = props.selectedEdge.to.y - props.selectedEdge.from.y
   let angle = Math.atan2(-dy, dx)
 
   const radius = 25
@@ -16,22 +16,22 @@ const getEdgeCenter = props => {
 
   const arrowStart = {
     x:
-      props.selectedArrow.from.x +
+      props.selectedEdge.from.x +
       -radius * Math.cos(angle + Math.PI) +
       (secondExist ? 5 * Math.cos(angle + Math.PI / 2) : 0),
     y:
-      props.selectedArrow.from.y +
+      props.selectedEdge.from.y +
       radius * Math.sin(angle + Math.PI) +
       (secondExist ? 5 * Math.sin(angle - Math.PI / 2) : 0),
   }
 
   const arrowEnd = {
     x:
-      props.selectedArrow.to.x +
+      props.selectedEdge.to.x +
       -radius * Math.cos(angle) +
       (secondExist ? 5 * Math.cos(angle + Math.PI / 2) : 0),
     y:
-      props.selectedArrow.to.y +
+      props.selectedEdge.to.y +
       radius * Math.sin(angle) +
       (secondExist ? 5 * Math.sin(angle - Math.PI / 2) : 0),
   }
@@ -52,16 +52,16 @@ export const Wrapper = styled.div`
   position: absolute;
   top: ${props => {
     if (props.selectedNode) {
-      return Number(props.selectedNode.y * props.scale + 85)
+      return Number(props.selectedNode.y * props.currentStageScale + 85)
     } else {
-      return Number(getEdgeCenter(props).y * props.scale + 85)
+      return Number(getEdgeCenter(props).y * props.currentStageScale + 85)
     }
   }}px;
   left: ${props => {
     if (props.selectedNode) {
-      return Number(props.selectedNode.x * props.scale + 40)
+      return Number(props.selectedNode.x * props.currentStageScale + 40)
     } else {
-      return Number(getEdgeCenter(props).x * props.scale + 30)
+      return Number(getEdgeCenter(props).x * props.currentStageScale + 30)
     }
   }}px;
 `
