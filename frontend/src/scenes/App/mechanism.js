568c02aa291ed0392101ddc5f1ffc07b0c6557e3
George Gkasdrogkas <georgegkas@gmail.com>
2019-09-22 22:38:06 +0300

    refactor: clean App mechanism

--- frontend/src/scenes/App/mechanism.js
+++ frontend/src/scenes/App/mechanism.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Presentation from './presentation'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { operations as editorOperations } from './organisms/Editor/duck'
-import { toast } from 'react-toastify'
 
 const mapStateToProps = state => ({
   selectedNode: state.editor.present.selectedNode,
@@ -15,65 +14,33 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch =>
   bindActionCreators(editorOperations, dispatch)
 
-@connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+const Mechanism = ({
+  isMultiSelect,
+  selectedNode,
+  editorActionType,
+  selectedArrow,
+}) => {
+  const [grid, makeGridVisible] = useState(false)
+  const [dashboard, makeDashboardVisible] = useState(false)
 
-    this.toggleGrid = this.toggleGrid.bind(this)
-    this.toggleDashboard = this.toggleDashboard.bind(this)
-    this.state = {
-      grid: false,
-      dashboard: false,
-    }
-  }
+  const toggleGrid = () => makeGridVisible(!grid)
+  const toggleDashboard = () => makeDashboardVisible(!dashboard)
 
-  toggleDashboard() {
-    if (this.state.dashboard) {
-      this.setState({
-        dashboard: false,
-      })
-    } else {
-      const requestOptions = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default',
-        credentials: 'same-origin',
-      }
-
-      fetch('/api/v1/auth/token', requestOptions).then(res => {
-        if (!res.ok) {
-          toast.error('Unauthoriazed action')
-        } else {
-          this.setState({
-            dashboard: true,
-          })
-        }
-      })
-    }
-  }
-
-  toggleGrid() {
-    this.setState({ grid: !this.state.grid })
-  }
-
-  render() {
-    return (
-      <Presentation
-        grid={this.state.grid}
-        toggleGrid={this.toggleGrid}
-        isMultiSelect={this.props.isMultiSelect}
-        selectedNode={this.props.selectedNode}
-        editorActionType={this.props.editorActionType}
-        selectedArrow={this.props.selectedArrow}
-        dashboard={this.state.dashboard}
-        toggleDashboard={this.toggleDashboard}
-      />
-    )
-  }
+  return (
+    <Presentation
+      grid={grid}
+      toggleGrid={toggleGrid}
+      isMultiSelect={isMultiSelect}
+      selectedNode={selectedNode}
+      editorActionType={editorActionType}
+      selectedArrow={selectedArrow}
+      dashboard={dashboard}
+      toggleDashboard={toggleDashboard}
+    />
+  )
 }
 
-export default App
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Mechanism)
