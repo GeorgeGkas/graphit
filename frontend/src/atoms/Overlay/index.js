2f0d51eb975933c5ff72a61abb54747d5db2151e
George Gkasdrogkas <georgegkas@gmail.com>
2019-09-23 14:55:25 +0300

    style: minor style update in Overlay atom

--- frontend/src/atoms/Overlay/index.js
+++ frontend/src/atoms/Overlay/index.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components'
 
 const Overlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.85);
   background: url(data:;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAgY0hSTQAAeiYAAICEAAD6AAAAgOgAAHUwAADqYAAAOpgAABdwnLpRPAAAABl0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjUuNUmK/OAAAAATSURBVBhXY2RgYNgHxGAAYuwDAA78AjwwRoQYAAAAAElFTkSuQmCC)
     repeat scroll transparent\9; /* ie fallback png background image */
+  background-color: rgba(0, 0, 0, 0.85);
+  bottom: 0;
+  color: #fff;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
   z-index: 9999;
-  color: white;
 `
 
 export default Overlay
