7dbca641c04b4d746672b9ba5a0824ebba8d87e5
George Gkasdrogkas <georgegkas@gmail.com>
2019-09-23 20:05:07 +0300

    refactor: load env variables

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,10 +2,3 @@
  * Load environmental variables
  */
 require('../index.js')
-
-/**
- * Add decorators support to React app.
- */
-const { override, addDecoratorsLegacy } = require('customize-cra')
-
-module.exports = override(addDecoratorsLegacy())
