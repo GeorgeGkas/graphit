6f2cfc168a345113bec3d19e2e18e1e87a042611
George Gkasdrogkas <georgegkas@gmail.com>
2019-10-07 16:02:52 +0300

    feat: lazy-load scenes

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,7 +1,8 @@
 /**
  * Import globals.
  */
-import React, { useEffect } from 'react'
+import loadable from '@loadable/component'
+import React, { Suspense, useEffect } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom'
@@ -15,11 +16,27 @@ import { compose } from 'redux'
 /**
  * Import components.
  */
-import App from './scenes/App'
-import Dashboard from './scenes/Dashboard'
-import Page404 from './scenes/Page404'
 import PrivateRoute from './organisms/PrivateRoute'
 import { withFirebase } from './organisms/Firebase'
+
+/**
+ * Lazy load scenes.
+ */
+const App = loadable(
+  () => import(/* webpackChunkName: "App" */ './scenes/App'),
+  {
+    fallback: <div>Loading...</div>,
+  },
+)
+const Dashboard = loadable(
+  () => import(/* webpackChunkName: "Dashboard" */ './scenes/Dashboard'),
+  {
+    fallback: <div>Loading...</div>,
+  },
+)
+const Page404 = loadable(() =>
+  import(/* webpackChunkName: "Page404" */ './scenes/Page404'),
+)
 
 /**
  * Connect component to Redux.
@@ -50,12 +67,18 @@ const Router = ({ firebase, signIn, signOut }) => {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route component={App} path="/app" />
-        <Redirect exact from="/" to="/app" />
-        <PrivateRoute component={Dashboard} fallback="/app" path="/dashboard" />
-        <Route component={Page404} />
-      </Switch>
+      <Suspense fallback={<div>Loading....</div>}>
+        <Switch>
+          <Route component={App} path="/app" />
+          <Redirect exact from="/" to="/app" />
+          <PrivateRoute
+            component={Dashboard}
+            fallback="/app"
+            path="/dashboard"
+          />
+          <Route component={Page404} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   )
 }
