453ff8e0ce2990c2e88899e262f2f1c35295a4fd
George Gkasdrogkas <georgegkas@gmail.com>
2019-10-22 11:46:48 +0300

    refactor: remove unsused dependencies

--- client/src/scenes/Dashboard/organisms/AppBar/index.js
+++ client/src/scenes/Dashboard/organisms/AppBar/index.js
@@ -2,17 +2,13 @@
  * Import globals.
  */
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 /**
  * Import UI framework modules.
  */
 import blue from '@material-ui/core/colors/blue'
-import DashboardIcon from '@material-ui/icons/DashboardSharp'
-import IconButton from '@material-ui/core/IconButton'
 import MUIAppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
