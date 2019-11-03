fdae318a2d79ebbe891c3e3901e4325afb985c67
George Gkasdrogkas <georgegkas@gmail.com>
2019-11-03 13:39:35 +0200

    feat: change page loading animation

--- client/src/organisms/Loading/index.js
+++ client/src/organisms/Loading/index.js
@@ -2,10 +2,22 @@
  * Import globals.
  */
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { ScaleLoader } from 'halogenium'
 
 /**
  * Component.
  */
-const Loading = () => <div>Loading...</div>
+const Loading = () => (
+  <Grid
+    container
+    alignItems="center"
+    direction="column"
+    justify="center"
+    style={{ minHeight: '100vh' }}
+  >
+    <ScaleLoader color="#2196f3" />
+  </Grid>
+)
 
 export default Loading
