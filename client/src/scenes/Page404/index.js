9f53c63d987b0932c4540927c77a21a84e17bb69
George Gkasdrogkas <georgegkas@gmail.com>
2019-11-10 12:46:57 +0200

    feat: update design

--- client/src/scenes/Page404/index.js
+++ client/src/scenes/Page404/index.js
@@ -5,43 +5,30 @@ import boat from './images/boat.svg'
 import island from './images/island.svg'
 
 const Page404 = () => (
-  <>
-    <div className="not-found parallax">
-      <div className="wave-7" />
-      <div className="wave-6" />
-      <Link className="wave-island" to="/">
-        <img alt="Island" src={island} />
-      </Link>
-      <div className="wave-5" />
-      <div className="wave-lost wrp">
-        <span>4</span>
-        <span>0</span>
-        <span>4</span>
-      </div>
-      <div className="wave-4" />
-      <div className="wave-boat">
-        <img alt="Boat" className="boat" src={boat} />
-      </div>
-      <div className="wave-3" />
-      <div className="wave-2" />
-      <div className="wave-1" />
-      <div className="wave-message">
-        <p>The destination does not exist.</p>
-        <p>Click on the island to return</p>
-      </div>
+  <div className="not-found parallax">
+    <div className="wave-7" />
+    <div className="wave-6" />
+    <Link className="wave-island" to="/">
+      <img alt="Island" src={island} />
+    </Link>
+    <div className="wave-5" />
+    <div className="wave-lost wrp">
+      <span>4</span>
+      <span>0</span>
+      <span>4</span>
     </div>
-    <p className="credit">
-      Page designed by{' '}
-      <a
-        href="https://codepen.io/andrew-lawendy"
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Andrew Lawendy
-      </a>
-      .
-    </p>
-  </>
+    <div className="wave-4" />
+    <div className="wave-boat">
+      <img alt="Boat" className="boat" src={boat} />
+    </div>
+    <div className="wave-3" />
+    <div className="wave-2" />
+    <div className="wave-1" />
+    <div className="wave-message">
+      <p>The destination does not exist.</p>
+      <p>Click on the island to return</p>
+    </div>
+  </div>
 )
 
 export default Page404
