73e0fcfdada918c6fd02348b81452a0b7ce5789a
George Gkasdrogkas <georgegkas@gmail.com>
2020-01-11 11:10:48 +0200

    fix: 404 component is shown in /app routes

--- client/src/scenes/Page404/index.js
+++ client/src/scenes/Page404/index.js
@@ -28,34 +28,36 @@ const Page404 = () => {
      */
     // eslint-disable-next-line no-restricted-globals
     history.replaceState({}, '', '/404')
-  }
 
-  return (
-    <div className="not-found parallax">
-      <div className="wave-7" />
-      <div className="wave-6" />
-      <Link className="wave-island" to="/">
-        <img alt="Island" src={island} />
-      </Link>
-      <div className="wave-5" />
-      <div className="wave-lost wrp">
-        <span>4</span>
-        <span>0</span>
-        <span>4</span>
+    return (
+      <div className="not-found parallax">
+        <div className="wave-7" />
+        <div className="wave-6" />
+        <Link className="wave-island" to="/">
+          <img alt="Island" src={island} />
+        </Link>
+        <div className="wave-5" />
+        <div className="wave-lost wrp">
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+        <div className="wave-4" />
+        <div className="wave-boat">
+          <img alt="Boat" className="boat" src={boat} />
+        </div>
+        <div className="wave-3" />
+        <div className="wave-2" />
+        <div className="wave-1" />
+        <div className="wave-message">
+          <p>{t('404.not_exist')}</p>
+          <p>{t('404.return_action')}</p>
+        </div>
       </div>
-      <div className="wave-4" />
-      <div className="wave-boat">
-        <img alt="Boat" className="boat" src={boat} />
-      </div>
-      <div className="wave-3" />
-      <div className="wave-2" />
-      <div className="wave-1" />
-      <div className="wave-message">
-        <p>{t('404.not_exist')}</p>
-        <p>{t('404.return_action')}</p>
-      </div>
-    </div>
-  )
+    )
+  } else {
+    return <></>
+  }
 }
 
 export default Page404
