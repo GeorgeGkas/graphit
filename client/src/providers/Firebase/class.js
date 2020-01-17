e999e0ae65de3fbe4173978e6c282069eb2347ae
George Gkasdrogkas <georgegkas@gmail.com>
2020-01-17 19:26:51 +0200

    feat: add google analytics

--- client/src/providers/Firebase/class.js
+++ client/src/providers/Firebase/class.js
@@ -1,11 +1,14 @@
 import 'firebase/auth'
+import 'firebase/analytics'
 
 import app from 'firebase/app'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  appId: process.env.REACT_APP_APP_ID,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -25,6 +28,15 @@ export default class Firebase {
 
     this.auth = app.auth()
     this.googleProvider = new app.auth.GoogleAuthProvider()
+
+    /**
+     * Initialize Google Analytics.
+     */
+    this.analytics = app.analytics()
+
+    /**
+     * TODO Report users that have disabled cookies and analytics
+     */
   }
 
   signIn = () => this.auth.signInWithPopup(this.googleProvider)
